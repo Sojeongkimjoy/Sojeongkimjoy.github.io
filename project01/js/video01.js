686290daff59e7f60243edcd07fae05904347645
Sojeongkimjoy <133853660+Sojeongkimjoy@users.noreply.github.com>
2023-05-24 16:13:01 +0900

    Add files via upload

--- project01/js/video01.js
+++ project01/js/video01.js
@@ -4,21 +4,19 @@ $(function () {
     $('.ctr').on('click', function () {
         $(this).toggleClass('on');
         if (btn) {
-            $('.case01 video').trigger('pause');
+            $('.main_visual video').trigger('pause');
         } else {
-            $('.case01 video').trigger('play');
+            $('.main_visual video').trigger('play');
         }
         btn = !btn;
         console.log(btn);
-
     })
 
     $('.case01 .pause').on('click', function () {
         $('.case01 video').trigger('pause');
-    })
-
+    });
     $('.case01 .play').on('click', function () {
         $('.case01 video').trigger('play');
-    })
+    });
 
-})
+});
